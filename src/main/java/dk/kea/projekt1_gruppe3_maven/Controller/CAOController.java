f0c141b396e44e749abdbd8b2a9717377a564c97
MagnusMunkBraunstein <mabr0004@stud.kea.dk>
2024-03-15 09:38:13 +0100

    Skift fra graddle til maven

--- src/main/java/dk/kea/projekt1_gruppe3_maven/Controller/CAOController.java
+++ src/main/java/dk/kea/projekt1_gruppe3_maven/Controller/CAOController.java
@@ -1,9 +1,12 @@
 package dk.kea.projekt1_gruppe3_maven.Controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+@Controller
 
-public class CompanyController {
-    @GetMapping("/company")
+public class CAOController {
+    @GetMapping("/Company")
     public String bmiCalculator(){ return "Company";
     }
 }
+
